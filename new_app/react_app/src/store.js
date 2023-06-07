0bd42795668edc4af5aeca349c161843fde3aff4
Shorya Malani <shoryamal@gmail.com>
2023-06-06 21:37:58 -0400

    reorienting folders

--- new_app/react_app/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import loggerSlice from './features/LoggerSlice'
-import appSlice from './features/AppSlice'
-export default configureStore({
-  reducer: {
-    logger: loggerSlice,
-    app: appSlice,
-  },
-})
