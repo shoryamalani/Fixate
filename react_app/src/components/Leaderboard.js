36b9e86ebfec2c798b3ee43313a586da467a0eb0
keckothedragon <flappystar05@gmail.com>
2023-06-16 23:18:16 -0400

    Fixed version numbers

--- react_app/src/components/Leaderboard.js
+++ react_app/src/components/Leaderboard.js
@@ -1,106 +1,346 @@
-import { Table } from "@mui/material";
-import { TableBody } from "@mui/material";
-import { TableCell } from "@mui/material";
-import { TableHead } from "@mui/material";
-import { TableRow } from "@mui/material";
-import { Paper } from "@mui/material";
+// import * as React from 'react';
+// import PropTypes from 'prop-types';
+// import { alpha } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TablePagination from '@mui/material/TablePagination';
+// import TableRow from '@mui/material/TableRow';
+// import TableSortLabel from '@mui/material/TableSortLabel';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Paper from '@mui/material/Paper';
+// import Checkbox from '@mui/material/Checkbox';
+// import IconButton from '@mui/material/IconButton';
+// import Tooltip from '@mui/material/Tooltip';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Switch from '@mui/material/Switch';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import FilterListIcon from '@mui/icons-material/FilterList';
+// import { visuallyHidden } from '@mui/utils';
 
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-      return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-      return 1;
-    }
-    return 0;
-  }
-  
-  function getComparator(order, orderBy) {
-    return order === 'desc'
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
-  }
-  const headCells = [
-    {
-      id: 'name',
-      numeric: false,
-      disablePadding: true,
-      label: 'Name',
-    },
-    {
-      id: 'timeSpent',
-      numeric: true,
-      disablePadding: false,
-      label: 'Time Spent',
-    },
-    {
-      id: 'percentFocused',
-      numeric: true,
-      disablePadding: false,
-      label: 'Percent Focused',
-    },
-    {
-      id: 'distractionsPerHour',
-      numeric: true,
-      disablePadding: false,
-      label: 'Distractions Per Hour',
-    },
-    {
-      id: 'longestTimeFocused',
-      numeric: true,
-      disablePadding: false,
-      label: 'Longest Time Focused',
-    },
-  ];
-  
-  // Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-  // stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-  // only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-  // with exampleArray.slice().sort(exampleComparator)
-  function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-      const order = comparator(a[0], b[0]);
-      if (order !== 0) {
-        return order;
-      }
-      return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-  }
-  
-  function Leaderboard(props) {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-      props;
-    const createSortHandler = (property) => (event) => {
-      onRequestSort(event, property);
-    };
-    return (
-        <>
-            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Application</TableCell>
-                            <TableCell align="right">Status</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {headCells.map((headCell) => (
-                                <TableCell 
-                                key={headCell.id}
-                                align={headCell.numeric ? 'right' : 'left'}
-                                padding={headCell.disablePadding ? 'none' : 'normal'}
-                                component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                {/* <TableCell align="right">{row.status}</TableCell> */}
-                                </TableRow>
 
-                        ))}
-                        </TableBody>
-                </Table>
-            </Paper>
-        </>
-    )
-}
+
+// function descendingComparator(a, b, orderBy) {
+//   if (b[orderBy] < a[orderBy]) {
+//     return -1;
+//   }
+//   if (b[orderBy] > a[orderBy]) {
+//     return 1;
+//   }
+//   return 0;
+// }
+
+// function getComparator(order, orderBy) {
+//   return order === 'desc'
+//     ? (a, b) => descendingComparator(a, b, orderBy)
+//     : (a, b) => -descendingComparator(a, b, orderBy);
+// }
+
+// // Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+// // stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+// // only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+// // with exampleArray.slice().sort(exampleComparator)
+// function stableSort(array, comparator) {
+//   const stabilizedThis = array.map((el, index) => [el, index]);
+//   stabilizedThis.sort((a, b) => {
+//     const order = comparator(a[0], b[0]);
+//     if (order !== 0) {
+//       return order;
+//     }
+//     return a[1] - b[1];
+//   });
+//   return stabilizedThis.map((el) => el[0]);
+// }
+
+// const headCells = [
+//   {
+//     id: 'name',
+//     numeric: false,
+//     disablePadding: true,
+//     label: 'Display Name',
+//   },
+//   {
+//     id: 'timeSpent',
+//     numeric: true,
+//     disablePadding: false,
+//     label: 'Time Spent Minutes',
+//   },
+//   {
+//     id: 'averageTimeBetweenDistractions',
+//     numeric: true,
+//     disablePadding: false,
+//     label: 'Average Time Between Distractions',
+//   },
+//   {
+//     id: 'numberOfDistractionsPerHour',
+//     numeric: true,
+//     disablePadding: false,
+//     label: 'Number of Distractions Per Hour',
+//   },
+//   {
+//     id: 'PercentTimeFocused',
+//     numeric: true,
+//     disablePadding: false,
+//     label: 'Percent Time Focused',
+//   },
+// ];
+
+// function Leaderboards(props) {
+//   const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+//     props;
+//   const createSortHandler = (property) => (event) => {
+//     onRequestSort(event, property);
+//   };
+
+//   return (
+//     <TableHead>
+//       <TableRow>
+//         <TableCell padding="checkbox">
+//           <Checkbox
+//             color="primary"
+//             indeterminate={numSelected > 0 && numSelected < rowCount}
+//             checked={rowCount > 0 && numSelected === rowCount}
+//             onChange={onSelectAllClick}
+//             inputProps={{
+//               'aria-label': 'select all desserts',
+//             }}
+//           />
+//         </TableCell>
+//         {headCells.map((headCell) => (
+//           <TableCell
+//             key={headCell.id}
+//             align={headCell.numeric ? 'right' : 'left'}
+//             padding={headCell.disablePadding ? 'none' : 'normal'}
+//             sortDirection={orderBy === headCell.id ? order : false}
+//           >
+//             <TableSortLabel
+//               active={orderBy === headCell.id}
+//               direction={orderBy === headCell.id ? order : 'asc'}
+//               onClick={createSortHandler(headCell.id)}
+//             >
+//               {headCell.label}
+//               {orderBy === headCell.id ? (
+//                 <Box component="span" sx={visuallyHidden}>
+//                   {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+//                 </Box>
+//               ) : null}
+//             </TableSortLabel>
+//           </TableCell>
+//         ))}
+//       </TableRow>
+//     </TableHead>
+//   );
+// }
+
+// Leaderboards.propTypes = {
+//   numSelected: PropTypes.number.isRequired,
+//   onRequestSort: PropTypes.func.isRequired,
+//   onSelectAllClick: PropTypes.func.isRequired,
+//   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+//   orderBy: PropTypes.string.isRequired,
+//   rowCount: PropTypes.number.isRequired,
+// };
+
+// function EnhancedTableToolbar(props) {
+//   const { numSelected } = props;
+
+//   return (
+//     <Toolbar
+//       sx={{
+//         pl: { sm: 2 },
+//         pr: { xs: 1, sm: 1 },
+//         ...(numSelected > 0 && {
+//           bgcolor: (theme) =>
+//             alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+//         }),
+//       }}
+//     >
+//       {numSelected > 0 ? (
+//         <Typography
+//           sx={{ flex: '1 1 100%' }}
+//           color="inherit"
+//           variant="subtitle1"
+//           component="div"
+//         >
+//           {numSelected} selected
+//         </Typography>
+//       ) : (
+//         <Typography
+//           sx={{ flex: '1 1 100%' }}
+//           variant="h6"
+//           id="tableTitle"
+//           component="div"
+//         >
+//           Nutrition
+//         </Typography>
+//       )}
+
+//       {numSelected > 0 ? (
+//         <Tooltip title="Delete">
+//           <IconButton>
+//             <DeleteIcon />
+//           </IconButton>
+//         </Tooltip>
+//       ) : (
+//         <Tooltip title="Filter list">
+//           <IconButton>
+//             <FilterListIcon />
+//           </IconButton>
+//         </Tooltip>
+//       )}
+//     </Toolbar>
+//   );
+// }
+
+// EnhancedTableToolbar.propTypes = {
+//   numSelected: PropTypes.number.isRequired,
+// };
+
+// export default function EnhancedTable() {
+//   const [order, setOrder] = React.useState('asc');
+//   const [orderBy, setOrderBy] = React.useState('calories');
+//   const [selected, setSelected] = React.useState([]);
+//   const [page, setPage] = React.useState(0);
+//   const [dense, setDense] = React.useState(false);
+//   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+//   const handleRequestSort = (event, property) => {
+//     const isAsc = orderBy === property && order === 'asc';
+//     setOrder(isAsc ? 'desc' : 'asc');
+//     setOrderBy(property);
+//   };
+
+// //   const handleSelectAllClick = (event) => {
+// //     if (event.target.checked) {
+// //       const newSelected = rows.map((n) => n.name);
+// //       setSelected(newSelected);
+// //       return;
+// //     }
+// //     setSelected([]);
+// //   };
+
+//   const handleClick = (event, name) => {
+//     const selectedIndex = selected.indexOf(name);
+//     let newSelected = [];
+
+//     if (selectedIndex === -1) {
+//       newSelected = newSelected.concat(selected, name);
+//     } else if (selectedIndex === 0) {
+//       newSelected = newSelected.concat(selected.slice(1));
+//     } else if (selectedIndex === selected.length - 1) {
+//       newSelected = newSelected.concat(selected.slice(0, -1));
+//     } else if (selectedIndex > 0) {
+//       newSelected = newSelected.concat(
+//         selected.slice(0, selectedIndex),
+//         selected.slice(selectedIndex + 1),
+//       );
+//     }
+
+//     setSelected(newSelected);
+//   };
+
+//   const handleChangePage = (event, newPage) => {
+//     setPage(newPage);
+//   };
+
+//   const handleChangeRowsPerPage = (event) => {
+//     setRowsPerPage(parseInt(event.target.value, 10));
+//     setPage(0);
+//   };
+
+//   const handleChangeDense = (event) => {
+//     setDense(event.target.checked);
+//   };
+
+//   return (
+//     <Box sx={{ width: '100%' }}>
+//       <Paper sx={{ width: '100%', mb: 2 }}>
+//         <EnhancedTableToolbar numSelected={selected.length} />
+//         <TableContainer>
+//           <Table
+//             sx={{ minWidth: 750 }}
+//             aria-labelledby="tableTitle"
+//             size={dense ? 'small' : 'medium'}
+//           >
+//             <Leaderboards
+//               numSelected={selected.length}
+//               order={order}
+//               orderBy={orderBy}
+//               onSelectAllClick={handleSelectAllClick}
+//               onRequestSort={handleRequestSort}
+//               rowCount={props.leaderboardData.length}
+//             />
+//             <TableBody>
+//               {visibleRows.map((row, index) => {
+//                 // const isItemSelected = isSelected(row.name);
+//                 // const labelId = `enhanced-table-checkbox-${index}`;
+
+//                 return (
+//                   <TableRow
+//                     hover
+//                     // onClick={(event) => handleClick(event, row.name)}
+//                     // role="checkbox"
+//                     // aria-checked={isItemSelected}
+//                     tabIndex={-1}
+//                     key={row.name}
+//                     // selected={isItemSelected}
+//                     sx={{ cursor: 'pointer' }}
+//                   >
+//                     <TableCell padding="checkbox">
+//                       {/* <Checkbox
+//                         color="primary"
+//                         checked={isItemSelected}
+//                         inputProps={{
+//                           'aria-labelledby': labelId,
+//                         }}
+//                       /> */}
+//                     </TableCell>
+//                     <TableCell
+//                       component="th"
+//                       id={row.id}
+//                       scope="row"
+//                       padding="none"
+//                     >
+//                       {row.name}
+//                     </TableCell>
+//                     <TableCell align="right">{row['total_time_spent']}</TableCell>
+//                     <TableCell align="right">{row['']}</TableCell>
+//                     <TableCell align="right">{row.carbs}</TableCell>
+//                     <TableCell align="right">{row.protein}</TableCell>
+//                   </TableRow>
+//                 );
+//               })}
+//               {emptyRows > 0 && (
+//                 <TableRow
+//                   style={{
+//                     height: (dense ? 33 : 53) * emptyRows,
+//                   }}
+//                 >
+//                   <TableCell colSpan={6} />
+//                 </TableRow>
+//               )}
+//             </TableBody>
+//           </Table>
+//         </TableContainer>
+//         <TablePagination
+//           rowsPerPageOptions={[5, 10, 25]}
+//           component="div"
+//           count={props.leaderboardData.length}
+//           rowsPerPage={rowsPerPage}
+//           page={page}
+//           onPageChange={handleChangePage}
+//           onRowsPerPageChange={handleChangeRowsPerPage}
+//         />
+//       </Paper>
+//       <FormControlLabel
+//         control={<Switch checked={dense} onChange={handleChangeDense} />}
+//         label="Dense padding"
+//       />
+//     </Box>
+//   );
+// }
+
