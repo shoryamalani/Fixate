2574f1b6e396a6c4647f37b70cbc23c1598a8cf7
keckothedragon <flappystar05@gmail.com>
2023-06-25 14:22:37 -0400

    Changed version numbers

---
+++ power_time_tracking_electron/installers/run-server.bat
@@ -0,0 +1,3 @@
+set PYTHONPATH=%PYTHONPATH%; %LocalAppData%\PowerTimeTracking\app-0.9.7\resources\app\src\python\Lib\site-packages
+set PYTHONHOME=%LocalAppData%\PowerTimeTracking\app-0.9.7\resources\app\src\python
+start "" "%LocalAppData%\PowerTimeTracking\app-0.9.7\resources\app\src\python\pythonw.exe" "%LocalAppData%\PowerTimeTracking\app-0.9.7\resources\app\src\py\server.py" "%1"
