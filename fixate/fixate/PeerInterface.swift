28a091f0a3f3230117de50fded8fa74c110137a2
Shorya Malani <shoryamal@gmail.com>
2024-09-18 18:46:18 -0400

    adding everything for fixing users

---
+++ fixate/fixate/PeerInterface.swift
@@ -0,0 +1,18 @@
+//
+//  PeerInterface.swift
+//  fixate
+//
+//  Created by Shorya Malani on 8/4/24.
+//
+
+import SwiftUI
+
+struct PeerInterface: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeerInterface()
+}
